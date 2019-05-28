741a52b5b8ddae81a4cbad0141791e469f6abf9e
Oğuzcan Demircan <demircanoguzcan@gmail.com>
2019-05-28 14:26:18 +0300

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 const resource = axios.create({
   baseURL: 'http://localhost/api/v1',
-  params:{
-    api_key: '19268149bbbd6ab148b5a6a6eabd2fce',
-    language:'tr-TR'
-  }
 });
 
 export default {
